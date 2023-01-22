8e3f3775dd5e31425114333fdf3bb479770f5db0
Nikolay Simakov <n.simakoff@gmail.com>
2023-01-22 09:21:32 +0300

    add pinia

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,13 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
-import store from "./store";
+import { createPinia } from 'pinia'
 import router from './router';
 
+const pinia = createPinia()
 const app = createApp(App);
 
 app.use(router);
-app.use(store);
+app.use(pinia);
 
 app.mount('#app');
