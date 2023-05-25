5d486fb08576bbc69c52f907d051d8920e9c8889
Nikolay Simakov <n.simakoff@gmail.com>
2023-05-25 21:35:51 +0300

    Update routes

--- server/src/api/routes/endpoints/links.py
+++ server/src/api/routes/endpoints/links.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from pydantic import ValidationError
 from datetime import datetime, timedelta
 from hashids import Hashids
 
@@ -14,24 +13,6 @@ router = APIRouter()
 
 @router.get(
     '/',
-    response_model=str,
-    name="Original URL",
-    description="Returns original link by token."
-)
-async def get_link(
-    token: str,
-    db: AsyncSession = Depends(get_session),
-):
-    if link := await links_crud.get_link(db, token):
-        return link.long_link
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=strings.LINK_DOES_NOT_EXIST
-        )
-
-
-@router.get(
-    '/all',
     name="All Links",
     description="""
     In future I'm going to separate this method for admins and other users. 
@@ -74,18 +55,10 @@ async def post_short_link(
         return await links_crud.create_link(db, link_data)
 
 
-@router.delete('/', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_link(
-    token: str | None = None,
-    db: AsyncSession = Depends(get_session),
-):
-    await links_crud.delete_link(db, token)
-    return None
-
-
 @router.delete(
-    '/all',
+    '/',
     status_code=status.HTTP_204_NO_CONTENT,
+    name="Delete all",
     description="Deletes all links. Dangerous!"
 )
 async def delete_links(
@@ -94,33 +67,23 @@ async def delete_links(
     return await links_crud.delete_links(db)
 
 
-@router.get('/{token}', response_model=Link, status_code=status.HTTP_200_OK)
+@router.get('/{token}', response_model=Link | str, status_code=status.HTTP_200_OK)
 async def get_link(
     token: str,
+    only_link: bool = False,
     db: AsyncSession = Depends(get_session),
 ):
     if link := await links_crud.get_link(db, token):
-        return link
+        if only_link:
+            return link.long_link  # returns orignal link
+        return link  # returns all link data
     else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=strings.LINK_DOES_NOT_EXIST
         )
 
 
-@router.get('/{token}/clicks', response_model=list[Click], status_code=status.HTTP_200_OK)
-async def get_link(
-    token: str,
-    db: AsyncSession = Depends(get_session),
-):
-    if (clicks := await clicks_crud.read_clicks(db, token)) != None:
-        return clicks
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=strings.LINK_DOES_NOT_EXIST
-        )
-
-
-@router.post('/{token}')
+@router.post('/{token}', name="Click")
 async def post_click(
     token: str,
     user_ip: str,
@@ -134,3 +97,25 @@ async def post_click(
     )
 
     return await clicks_crud.create_click(db, click_data)
+
+
+@router.delete('/{token}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_link(
+    token: str,
+    db: AsyncSession = Depends(get_session),
+):
+    await links_crud.delete_link(db, token)
+    return None
+
+
+@router.get('/{token}/clicks', response_model=list[Click], status_code=status.HTTP_200_OK)
+async def get_link_clicks(
+    token: str,
+    db: AsyncSession = Depends(get_session),
+):
+    if (clicks := await clicks_crud.read_clicks(db, token)) != None:
+        return clicks
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=strings.LINK_DOES_NOT_EXIST
+        )
