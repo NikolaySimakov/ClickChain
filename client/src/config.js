98a78ae43dbd8dd79bc13b29e1c9c3515c325d88
Nikolay Simakov <n.simakoff@gmail.com>
2023-06-01 10:10:23 +0300

    Add domain to config

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
 export const CONFIG = {
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  DOMAIN: import.meta.env.VITE_DOMAIN || 'http://127.0.0.1:5173/',
   API_HOST: import.meta.env.VITE_API_HOST || 'http://localhost:3002',
 }
