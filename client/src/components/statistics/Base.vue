f3ba8521e09c7d97160b77c23ea6c876a73021ed
Nikolay Simakov <n.simakoff@gmail.com>
2023-08-16 23:47:30 +0300

    Add props in base statistics component

--- client/src/components/statistics/Base.vue
+++ client/src/components/statistics/Base.vue
@@ -2,23 +2,23 @@
   <div class="border-4 border-black w-fit p-4 mt-20">
     <div class="my-2">
       <p class="font-medium">Total Clicks</p>
-      <h3 class="font-bold text-4xl">{{ totalClicks }}</h3>
+      <h3 class="font-bold text-4xl">{{ props.totalClicks }}</h3>
     </div>
     <div class="my-2">
       <p class="font-medium">Average Per Day</p>
-      <h3 class="font-bold text-4xl">{{ averagePerDay }}</h3>
+      <h3 class="font-bold text-4xl">{{ props.averagePerDay }}</h3>
     </div>
     <div class="my-2">
       <p class="font-medium">Unique Clicks</p>
-      <h3 class="font-bold text-4xl">{{ uniqueClicks }}</h3>
+      <h3 class="font-bold text-4xl">{{ props.uniqueClicks }}</h3>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-const totalClicks = ref(0);
-const averagePerDay = ref(0);
-const uniqueClicks = ref(0);
+const props = defineProps({
+  totalClicks: Number,
+  averagePerDay: Number,
+  uniqueClicks: Number,
+});
 </script>
