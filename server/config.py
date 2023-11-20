69dc657b7d3be545c73f6f8f05fcdf4ea44af6e2
NikolaySimakov <n.simakoff@gmail.com>
2023-11-20 23:46:26 +0300

    REMOVE: config file

--- server/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DATABASE_URL = os.environ.get('DATABASE_URL')
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
